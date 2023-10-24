efd6d05a5048877cb8646d3e17a7a39fe8441a63
LucasJoseArantes <132081624+LucasJoseArantes@users.noreply.github.com>
2023-10-23 21:58:30 -0300

    Add files via upload

---
+++ jdbc-mysql/src/entities/Product.java
@@ -0,0 +1,61 @@
+package entities;
+
+public class Product {
+    private Long id;
+    private String name;
+    private Double price;
+    private String desc;
+    private String imageUri;
+
+    public Product(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getImageUri() {
+        return imageUri;
+    }
+
+    public void setImageUri(String imageUri) {
+        this.imageUri = imageUri;
+    }
+
+    @Override
+    public String toString() {
+        return "Product [id=" + id + ", name=" + name + ", price=" + price + ", desc=" + desc + ", imageUri=" + imageUri
+                + "]";
+    }
+
+    
+}
